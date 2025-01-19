b5406bd18bc9af2f72a248acf9b5cf5c9ac69ffa
Arman Hossen <145398391+1ahrn@users.noreply.github.com>
2025-01-19 19:24:23 +0600

    FS

--- if_else2.c
+++ if_else2.c
@@ -8,6 +8,7 @@ int main()
     if (num % 5 == 0 && num % 11 == 0)
     { // here the '%' modulus operator finds the remainder, if the number is divided by 5,11 then the reminder will be zero,
       // it will be printed when the condition is true
+      //&& works for AND logic operator , when both condition is true then it make the condition true
         printf("Number is divisible by 5 and 11\n");
     }
     else
