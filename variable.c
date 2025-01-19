1edb65aad81064a2c232da9f1a423dd62e20013f
Arman Hossen <145398391+1ahrn@users.noreply.github.com>
2025-01-19 17:22:27 +0600

    FarhanSadik

---
+++ variable.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a, b; // a, b are integer variables
+    int sum = 0; //sum is also integr variable with assign value zero
+    printf("Enter two number to sum: ");
+    scanf("%d%d",&a,&b); // scanf function to take input from output screen, %d for taking only integer value or part, and &a means address of a, the input will be assigned in the address of a
+    sum = a+b; // integer value of  a & b and the summation is signed in the variable sum
+    printf("Sum = %d\n",sum); //%d is the format specifier for displaying and retrieving integer variables
+    return 0;
+}
