51dd20115de50fcd6ad2aff4577901cf814c463a
Arman Hossen <145398391+1ahrn@users.noreply.github.com>
2025-01-19 19:44:22 +0600

    FS

--- forloop.c
+++ forloop.c
@@ -1,14 +1,25 @@
-//C program to print all even numbers from 1 to n
-#include<stdio.h>
-int main(){
+// C program to print all even numbers from 1 to n
+#include <stdio.h>
+int main()
+{
     int n;
-    scanf("%d",&n);    
-    //for (initialization; condition; increment or decrement) {
-        //code block to be executed
+    printf("Input upper range: ");
+    scanf("%d", &n);
+    // for (initialization; condition; increment or decrement) {
+    // code block to be executed
     //}
-    for(int i = 1; i <= n; i++){
-        if(i%2==0){ //condition to check if the number is even or not
-            printf("%d ",i); //if condition true then it prints the number
+    printf("Even numbers between 1 to %d: ", n);
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {                    // condition to check if the number is even or not
+            printf("%d", i); // if condition true then it prints the number
+
+            if (i != n && i > 1)
+            { // after the number print when it is not the last or first number
+              // this will print ','
+                printf(", ");
+            }
         }
     }
     return 0;
