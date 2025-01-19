e2604e25ce027da9bc14d9e22f883b0d8289a3de
Arman Hossen <145398391+1ahrn@users.noreply.github.com>
2025-01-19 19:39:20 +0600

    FS

--- forloop.c
+++ forloop.c
@@ -2,9 +2,11 @@
 #include<stdio.h>
 int main(){
     int n;
-    scanf("%d",&n);
+    scanf("%d",&n);    
+    //for (initialization; condition; increment or decrement) {
+        //code block to be executed
+    //}
     for(int i = 1; i <= n; i++){
-    //for(initial; condition, increment or decrement)
         if(i%2==0){ //condition to check if the number is even or not
             printf("%d ",i); //if condition true then it prints the number
         }
